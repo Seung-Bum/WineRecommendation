019bb9399b97321499f47a381120e1aada31a2b3
Seung-Bum <sb910126@gmail.com>
2025-01-05 16:40:27 +0900

    add ssl_context

--- calculate.py
+++ calculate.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -8,16 +8,16 @@ def index():
     return render_template('index.html')
 
 
-# @app.route('/calculate', methods=['GET'])
-@app.route('/calculate')
+# @app.route('/calculate')
+@app.route('/calculate', methods=['POST'])
 def calculate():
     data = request.json
     try:
         num1 = float(data['num1'])
         num2 = float(data['num2'])
     except ValueError:
-        # return jsonify({'error': 'Invalid number format'}), 400
-        return render_template("report.html", result='Invalid number format')
+        return jsonify({'error': 'Invalid number format'}), 400
+        # return render_template("report.html", result='Invalid number format')
 
     num1 = float(data.get('num1'))
     num2 = float(data.get('num2'))
@@ -31,16 +31,17 @@ def calculate():
         result = num1 * num2
     elif operation == 'divide':
         if num2 == 0:
-            # eturn jsonify({'error': 'Cannot divide by zero'}), 400
-            return render_template("report.html", result='Cannot divide by zero')
+            return jsonify({'error': 'Cannot divide by zero'}), 400
+            # return render_template("report.html", result='Cannot divide by zero')
         result = num1 / num2
     else:
-        # return jsonify({'error': 'Invalid operation'}), 400
-        return render_template("report.html", result='Invalid operation')
+        return jsonify({'error': 'Invalid operation'}), 400
+        # return render_template("report.html", result='Invalid operation')
 
-    # return jsonify({'result': result})
-    return render_template("report.html", result=result)
+    return jsonify({'result': result})
+    # return render_template("report.html", result=result)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True,
+            ssl_context=('cert.pem', 'key.pem'))
